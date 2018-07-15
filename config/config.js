90e6d0e0c03174a50ea809b63089312542cc8111
anurakt27 <31899581+anurakt27@users.noreply.github.com>
2018-07-15 20:19:20 +0530

    Update config.js

--- config/config.js
+++ config/config.js
@@ -6,7 +6,13 @@ var init = function () {
 		var redisURI 		= require('url').parse(process.env.REDIS_URL);
 		var redisPassword 	= redisURI.auth.split(':')[1];
 		return {
-			db: process.env.MONGODB_URI,
+			db: {
+				username: process.env.dbUsername,
+				password: process.env.dbPassword,
+				host: process.env.dbHost,
+				port: process.env.dbPort,
+				name: process.env.dbName
+			},
 			sessionSecret: process.env.sessionSecret,
 			redis: {
 				host: redisURI.hostname,
