f2c1675393b5d5d37a45ee99691a84ebdaeb4eb6
anurakt27 <31899581+anurakt27@users.noreply.github.com>
2018-07-15 17:03:29 +0530

    Update session.js

--- config/session.js
+++ config/session.js
@@ -3,11 +3,12 @@
 var session 	= require('express-session');
 var MongoStore	= require('connect-mongo')(session);
 var db 		    = require('./database');
+var config 	=require('./config');
 
 var init = function () {
 	if(process.env.NODE_ENV === 'production') {
 		return session({
-			secret: 'SECRET',
+			secret: config.sessionSecret,
 			resave: false,
 			saveUninitialized: false,
 			unset: 'destroy',
